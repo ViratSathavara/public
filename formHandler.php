65a152b86126f7bd6bd4c9bf9fbc92972fd3e0bc
Virat Sathavara <viratmsathavara2510@gmail.com>
2023-06-05 13:09:42 +0530

    changes commited

---
+++ formHandler.php
@@ -0,0 +1,28 @@
+
+<?php
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$email_from = 'info@viratblogsite.web.app';
+
+$email_subject = 'New Form Submission';
+
+$email_body = "User Name: $name.\n".
+              "User Email: $visitor_email.\n".
+              "Subject: $subject.\n".
+              "User Message: $message .\n";
+
+$to = 'viratmsathavara2510@gmail.com';
+
+$headers = "From: $email_from \r\n";
+
+$headers = "Reply-To: $visitor_email \r\n";
+
+header("Location: contact.html");
+
+mail($to,$email_subject,$email_body,$headers);
+
+
+?>
